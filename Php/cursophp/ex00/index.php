efccfce57d7f0f0d8ed70e51a4c38bb86db46610
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-04-24 19:51:02 -0300

    2.0

---
+++ Php/cursophp/ex00/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body><h1>
+    <?php 
+    echo "ola mundo";
+    ?>
+</body>
+</html>
